fafe0543ce53a654a56f39756b314a51623a3494
Rena Goldsmith <rena.kg@gmail.com>
2016-05-04 17:29:08 +1200

    hide race tracks

--- js/events.js
+++ js/events.js
@@ -1,28 +1,28 @@
 // Rules pop up preview window
 var howToPlay = document.getElementById('rules');
-howToPlay.addEventListener("mouseover", showRules);
+howToPlay.addEventListener("click", showRules);
   function showRules(){
-    toolTip = document.querySelector(".container .row .tooltipHow span.tooltiptextHow")
+    toolTip = document.querySelector(".container .row .tooltipHow span.tooltiptextHow");
     toolTip.classList.add("show");
   }
 
 var leavehowToPlay = document.getElementById('rules');
 leavehowToPlay.addEventListener("mouseleave", hideRules);
   function hideRules(){
-    toolTip = document.querySelector(".container .row .tooltipHow span.tooltiptextHow.show")
+    toolTip = document.querySelector(".container .row .tooltipHow span.tooltiptextHow");
     toolTip.classList.remove("show");
   }
 
 //set Players
 var choosePlayers = document.getElementById('setPlayers');
-choosePlayers.addEventListener("mouseover", showPlayers);
+choosePlayers.addEventListener("click", showPlayers);
   function showPlayers(){
-    toolTip = document.querySelector(".container .row .tooltipPlayers span.tooltiptextPlayers")
+    toolTip = document.querySelector(".container .row .tooltipPlayers span.tooltiptextPlayers");
     toolTip.classList.add("show");
   }
 
 var leavechoosePlayers = document.getElementById('setPlayers');
-leavechoosePlayers.addEventListener("mouseleave", hidePlayers);
+leavechoosePlayers.addEventListener("click", showPlayers);
   function hidePlayers(){
     toolTip = document.querySelector(".container .row .tooltipPlayers span.tooltiptextPlayers.show")
     toolTip.classList.remove("show");
