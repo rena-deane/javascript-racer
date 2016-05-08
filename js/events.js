438c98040f158e3cf6e96fc7771e8c56594018f7
Rena Goldsmith <rena.kg@gmail.com>
2016-05-08 21:49:09 +1200

    choose player tracks max 4

--- js/events.js
+++ js/events.js
@@ -14,5 +14,37 @@ leavehowToPlay.addEventListener("mouseleave", hideRules);
   }
 
 //set Players
+var get1Player = document.getElementById('player1');
+get1Player.addEventListener("click", function(){
+    document.getElementById('player1_strip').style.display = "inherit";
+    document.getElementById('player2_strip').style.display = "none";
+    document.getElementById('player3_strip').style.display = "none";
+    document.getElementById('player4_strip').style.display = "none";
+  });
+
+var get2Player = document.getElementById('player2');
+get2Player.addEventListener("click", function(){
+    document.getElementById('player1_strip').style.display = "inherit";
+    document.getElementById('player2_strip').style.display = "inherit";
+    document.getElementById('player3_strip').style.display = "none";
+    document.getElementById('player4_strip').style.display = "none";
+  });
+
+var get3Player = document.getElementById('player3');
+get3Player.addEventListener("click", function(){
+    document.getElementById('player1_strip').style.display = "inherit";
+    document.getElementById('player2_strip').style.display = "inherit";
+    document.getElementById('player3_strip').style.display = "inherit";
+    document.getElementById('player4_strip').style.display = "none";
+  });
+
+var get4Player = document.getElementById('player4');
+get4Player.addEventListener("click", function(){
+    document.getElementById('player2_strip').style.display = "inherit";
+    document.getElementById('player1_strip').style.display = "inherit";
+    document.getElementById('player3_strip').style.display = "inherit";
+    document.getElementById('player4_strip').style.display = "inherit";
+  });
+
 
 // get player strips and tooltip options
